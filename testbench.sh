6a7c148cfe2f83d89f6415a59dfc3e1a58fa73bc
chenIshi <chen.ishi@gmail.com>
2018-12-27 15:32:57 +0800

    Add support to gcc -O3 sse2
    In gcc optimization -O3 highest level, it will try to use vector
    data in sse to boost performance

--- testbench.sh
+++ testbench.sh
@@ -11,6 +11,9 @@ FIXED_PLOT_DATA_SSE="./fixed_plot_data_sse.txt"
 FIXED_TESTBENCH_WITHOUT_SSE="./fixed_testbench_without_sse"
 FIXED_PLOT_DATA_WITHOUT_SSE="./fixed_plot_data_without_sse.txt"
 
+FLOAT_TESTBENCH_GCC_SSE2="./float_with_gcc_sse2"
+FLOAT_PLOT_DATA_GCC_SSE2="./float_plot_data_gcc_sse2.txt"
+
 SP_TESTBENCH="./special_testbench"
 SP_PLOT_DATA="./special_plot_data.txt"
 
@@ -40,9 +43,15 @@ then
     rm $FIXED_PLOT_DATA_WITHOUT_SSE
 fi
 
+if [ -a $FLOAT_PLOT_DATA_GCC_SSE2 ]
+then
+    rm $FLOAT_PLOT_DATA_GCC_SSE2
+fi
+
 # testbench
 $FLOAT_TESTBENCH >> $FLOAT_PLOT_DATA
 $FIXED_TESTBENCH >> $FIXED_PLOT_DATA 
 $SP_TESTBENCH >> $SP_PLOT_DATA
 $FIXED_TESTBENCH_SSE >> $FIXED_PLOT_DATA_SSE
 $FIXED_TESTBENCH_WITHOUT_SSE >> $FIXED_PLOT_DATA_WITHOUT_SSE
+$FLOAT_TESTBENCH_GCC_SSE2 >> $FIXED_PLOT_DATA_WITHOUT_SSE
